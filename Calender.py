ee2eef2bdb190c80269c6ab6dabe93ac071301d4
shindepriti <47653685+shindepriti@users.noreply.github.com>
2019-09-04 10:11:05 +0530

    Add files via upload

--- Calender.py
+++ Calender.py
@@ -1,4 +1,11 @@
-import calendar
-c=calendar.TextCalendar(calendar.SUNDAY)
-str=c.formatmonth(2019,8)
-print(str)
+def maxno(num):
+ max=num[0]
+ min=num[0]
+ for data in num:
+    if data > max:
+        max=num
+    elif data < min:
+        min=data
+
+ return max,min
+print(maxno([9,6,7,4]))
